d84225847422a80c242a82bd532a740a1094a0c0
Serhat Uludag <serhat3264@gmail.com>
2024-07-17 18:29:40 +0300

    Restart Algorithm Finished, and Timer fixed

--- app/src/main/java/com/serhatuludag/catchthekennykotlin/GameScreen.kt
+++ app/src/main/java/com/serhatuludag/catchthekennykotlin/GameScreen.kt
@@ -1,9 +1,12 @@
 package com.serhatuludag.catchthekennykotlin
 
+import android.content.DialogInterface
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -21,18 +24,53 @@ class GameScreen : AppCompatActivity() {
         enableEdgeToEdge()
         binding = ActivityGameScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        timer()
+        startGame()
     }
 
     fun timer(){
         runnable = object : Runnable {
             override fun run() {
                 time--
+
                 binding.timeText.text = "Time : $time"
-                handler.postDelayed(runnable, 1000)
+                if(time>0){
+                    handler.postDelayed(runnable, 1000)
+                }else{
+                    handler.removeCallbacks(runnable)
+                    restartAlert()
+                }
+
             }
         }
         handler.post(runnable)
     }
+    fun startGame(){
+        time = 16
+        binding.timeText.text = "Time : $time"
+        score = 0
+        timer()
+
+    }
+
+    fun restartAlert(){
+        //Alert and Ask user to restart the game when time is over
+        val alert = AlertDialog.Builder(this)
+        alert.setTitle("Restart")
+        alert.setMessage("Dou you want to restart the game?")
+        alert.setPositiveButton("Yes",object : DialogInterface.OnClickListener{
+            override fun onClick(p0: DialogInterface?, p1: Int) {
+                startGame()
+            }
+        })
+        alert.setNegativeButton("No",object : DialogInterface.OnClickListener{
+            override fun onClick(p0: DialogInterface?, p1: Int) {
+                Toast.makeText(this@GameScreen,"Game Over",Toast.LENGTH_LONG).show()
+            }
+        })
+        alert.show()
+
+    }
+
+
 
 }
